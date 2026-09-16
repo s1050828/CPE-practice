a65b147368537692f94dfdc26573bee929c74ea4
s1050828 <138659004+s1050828@users.noreply.github.com>
2026-09-16 11:43:28 +0800

    Add files via upload

---
+++ 2 star/DNA_Sorting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int T;
+    cin >> T;
+    cin.ignore();
+
+    for (int i = 0; i < T; i++){
+        string line;
+        getline(cin,line);
+        
+        int m,n;
+        cin >> m >>n;
+        
+    }
+    
+
+}
