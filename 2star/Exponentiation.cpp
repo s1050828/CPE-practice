e3ad59f02ded72f92aa4d4a768b7fe805786f6de
s1050828 <s1050828.chen@gmail.com>
2026-09-23 12:30:09 +0800

    complete exponentiation

--- 2star/Exponentiation.cpp
+++ 2star/Exponentiation.cpp
@@ -1,33 +1,53 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <cmath>
 using namespace std;
-void mul(const vector<int> &I,long long m){
-
+void mul(vector<int> &I,long long m){
+    long long int carry = 0;
+    for (auto &i : I){
+        long long cur = i * m + carry;
+        i = cur % 10;
+        carry = cur / 10;
+    }
+    while(carry){
+        I.push_back(carry%10);
+        carry/=10;
+    }
 }
 
 int main(){
     string line;
-    while(getline(cin,line)){
-        if(line.size()>9){
+    while(getline(cin,line) && line!=""){
+        if(line.size()>10){
             cout << "Invalid Input the input size = " << line.size()<<endl;
             return 1;
         }
-        string R = line.substr(0,6);
-        int n = stoi(line.substr(7,2));
-        size_t dot =R.find('.');
-        if(dot == string::npos) dot = -1;
-        else R.erase(dot, 1);
+        string R = line.substr(0, 6);
+        int n = stoi(line.substr(7));
+        R.erase(remove(R.begin(), R.end(), ' '), R.end());   
 
+        int dec = 0;                                        
+        size_t dot = R.find('.');
+        if (dot != string::npos){
+            dec = R.size() - dot - 1;
+            R.erase(dot, 1);
+        }
         long long m = stoll(R);
+        while (dec > 0 && m % 10 == 0){ m /= 10; dec--; }    
 
         vector<int> output = {1};
-        for (int i = 0 ; i < n ; i ++) mul(output,m);
-        
-        int times = output.size();
-        for  (int i = 0 ; i < times; i ++) {
-            if(i == dot){ cout <<'.'; times++;}
-            else cout << output[i];
+        for (int i = 0; i < n; i++) mul(output, m);
+        reverse(output.begin(), output.end());
+
+        string s;
+        for (int d : output) s += char('0' + d);
+        int total = dec * n;                                  // 結果的小數位
+        if (total > 0){
+            if ((int)s.size() < total) s = string(total - s.size(), '0') + s; 
+            s.insert(s.size() - total, ".");
         }
+        cout << s << "\n";
     }
 }
